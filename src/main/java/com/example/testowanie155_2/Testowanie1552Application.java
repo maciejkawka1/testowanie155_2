d39af988fdde4f2046839e19cc0ca31b70f102cb
maciejkawka1 <kawkamaciej.mk@gmail.com>
2024-06-10 20:31:57 +0200

    init commit

---
+++ src/main/java/com/example/testowanie155_2/Testowanie1552Application.java
@@ -0,0 +1,13 @@
+package com.example.testowanie155_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testowanie1552Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testowanie1552Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testowanie155_2/Testowanie1552ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testowanie155_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testowanie1552ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
