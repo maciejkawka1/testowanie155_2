04f5badab4a7cd90740b67bdb459cee8e20dfd13
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-11 19:56:16 +0200

    repository testing

---
+++ src/main/java/com/example/testowanie155_2/entity/Book.java
@@ -0,0 +1,20 @@
+package com.example.testowanie155_2.entity;
+
+import java.math.BigDecimal;
+// test zapisywania ksiazki
+// test szukania wszystkich ksiązek
+// test uaktualnainia ksiązki
+// test usuwania ksiazki
+// test wyszukiwania po gatunku
+// test wyszukiwania po autorze i dacie publikacji - pozytywny scenariusz
+// test wyszukiwania po autorze i dacie publikacji - negatywny scenariusz
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int publishedYear;
+    private String genre;
+    private BigDecimal price;
+    private Boolean available;
+
+}
