852f620c0629ba599f668172df64e7652c5d6f13
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-11 17:46:19 +0200

    repository testing

--- src/main/java/com/example/testowanie155_2/repository/EmployeeRepository.java
+++ src/main/java/com/example/testowanie155_2/repository/EmployeeRepository.java
@@ -3,5 +3,6 @@ package com.example.testowanie155_2.repository;
 import com.example.testowanie155_2.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
