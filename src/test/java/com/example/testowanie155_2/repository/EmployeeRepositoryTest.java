a83c7de0a42c33fb8106faa718b064bbe9955bce
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-11 17:57:13 +0200

    repository testing

--- src/test/java/com/example/testowanie155_2/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/example/testowanie155_2/repository/EmployeeRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest // domyślnie używa do testowania bazy in-memory, testy są wykonywane w ramach transakcji i po testach jest wykonywany 'roll back'
@@ -20,13 +22,40 @@ class EmployeeRepositoryTest {
     private EmployeeRepository employeeRepository;
     @Test
     @DisplayName("Zapisywanie pracownika")
-    void x() {
+    void givenEmployee_whenSave_thenReturnSavedEmployee() {
         // given
         Employee employee = new Employee();
+
         // when
         Employee save = employeeRepository.save(employee);
 
         // then
         assertThat(save).isNotNull();
+        assertThat(save.getId()).isGreaterThan(0);
+        assertThat(save).isEqualTo(employee);
     }
+
+    @Test
+    @DisplayName("Pobieranie pracowników")
+    void givenTwoEmployees_whenFindAll_thenReturnEmployeeList() {
+        // given
+        Employee employee = new Employee();
+        Employee employee2 = new Employee();
+        employeeRepository.save(employee);
+        employeeRepository.save(employee2);
+
+        // when
+        List<Employee> employees = employeeRepository.findAll();
+
+        // then
+        assertThat(employees).isNotNull();
+        assertThat(employees.size()).isEqualTo(2);
+    }
+    // pobieranie pracownika po id
+    // pobieranie pracownika po email
+    // uaktualnienie danych pracownika
+    // usuwanie pracownika
+    // znajdowanie pracownika po imieniu i nazwisku
+
+
 }
