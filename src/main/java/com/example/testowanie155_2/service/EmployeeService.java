ef73d504991c4333ea451771bede4d54491f1ab9
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-12 18:50:28 +0200

    repository testing

---
+++ src/main/java/com/example/testowanie155_2/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.example.testowanie155_2.service;
+
+import com.example.testowanie155_2.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+}
---
+++ src/test/java/com/example/testowanie155_2/service/EmployeeServiceTest.java
@@ -0,0 +1,17 @@
+package com.example.testowanie155_2.service;
+
+import com.example.testowanie155_2.repository.EmployeeRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceTest {
+    @Mock
+    private EmployeeRepository employeeRepository;
+    @InjectMocks
+    private EmployeeService employeeService;
+}
